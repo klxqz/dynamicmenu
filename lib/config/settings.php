90915be9f7c43b38bcedb468b6ec4094c86734de
kl <kl@XSTREAM>
2013-11-22 00:57:44 +0600

    next 22.11.2013 0:57

--- lib/config/settings.php
+++ lib/config/settings.php
@@ -11,7 +11,6 @@ return array(
             '1' => 'Включен',
         )
     ),
-    
     'default_output' => array(
         'title' => 'Вывод в стандартном месте',
         'description' => '',
@@ -22,7 +21,6 @@ return array(
             '1' => 'Включен',
         )
     ),
-    
     'typemenu' => array(
         'title' => 'Тип меню',
         'description' => '',
@@ -34,8 +32,65 @@ return array(
             'navbar' => 'Навигационная панель',
         )
     ),
+    'delay' => array(
+        'title' => 'Задержка',
+        'description' => 'Задержка в миллисекундах перед скрытием подменю после того как курсор будет убран',
+        'value' => '800',
+        'control_type' => waHtmlControl::INPUT,
+    ),
+    'speed' => array(
+        'title' => 'Скорость анимации',
+        'description' => '',
+        'value' => 'normal',
+        'control_type' => waHtmlControl::SELECT,
+        'options' => array(
+            'slow' => 'Медленно',
+            'normal' => 'Средне',
+            'fast' => 'Быстро',
+        )
+    ),
+    'speedOut' => array(
+        'title' => 'Скорость анимации при закрывание',
+        'description' => '',
+        'value' => 'fast',
+        'control_type' => waHtmlControl::SELECT,
+        'options' => array(
+            'slow' => 'Медленно',
+            'normal' => 'Средне',
+            'fast' => 'Быстро',
+        )
+    ),
+    'autoArrows' => array(
+        'title' => 'Стрелка',
+        'description' => 'Если Да, то стрелка указывающая наличие подменю будет показана',
+        'value' => 'true',
+        'control_type' => waHtmlControl::SELECT,
+        'options' => array(
+            'true' => 'Да',
+            'false' => 'Нет',
+        )
+    ),
     
     
+    'sfMenuLi' => array(
+        'title' => 'Цвет фона',
+        'description' => 'Цвет в шестнадцатиричном виде #000000, http://getcolor.ru/',
+        'value' => '#BDD2FF',
+        'control_type' => waHtmlControl::INPUT,
+    ),
     
+    'sfMenuA' => array(
+        'title' => 'Цвет текста',
+        'description' => 'Цвет в шестнадцатиричном виде #000000, http://getcolor.ru/',
+        'value' => '#1133AA',
+        'control_type' => waHtmlControl::INPUT,
+    ),
+    
+    'sfMenuAHover' => array(
+        'title' => 'Цвет текста при наведении',
+        'description' => 'Цвет в шестнадцатиричном виде #000000, http://getcolor.ru/',
+        'value' => '#1133AA',
+        'control_type' => waHtmlControl::INPUT,
+    ),
     
 );
